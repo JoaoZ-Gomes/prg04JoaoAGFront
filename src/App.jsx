26df4948d9242375feec4f7918ba269669b0fce6
JoaoZ-Gomes <jgjoaogomespro@gmail.com>
2025-11-27 00:01:53 -0300

    Adiciona TrainingCreator (componente e estilos) e rota /consultor/criar-treino

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
+import Profile from './pages/Profile'
+import ConsultantDashboard from './pages/ConsultantDashboard'
+import TrainingCreator from './pages/TrainingCreator'
 import './App.css'
 
 function AppContent() {
   const location = useLocation()
-  const isLoginPage = location.pathname === '/login' || location.pathname === '/cadastro'
+  const isLoginPage =
+    location.pathname === '/login' ||
+    location.pathname === '/cadastro' ||
+    location.pathname.startsWith('/consultor')
 
   return (
     <>
@@ -18,6 +24,9 @@ function AppContent() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/perfil" element={<Profile />} />
+          <Route path="/consultor/dashboard" element={<ConsultantDashboard />} />
+          <Route path="/consultor/criar-treino" element={<TrainingCreator />} />
         </Routes>
       </main>
       {!isLoginPage && <Footer />}
